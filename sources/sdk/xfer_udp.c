80ae29a144947c04aceb9b261b6a8959c78de2e6
Leonardo Lanante <leonardolanante@gmail.com>
2020-07-23 05:40:12 -0700

    Save WIP

--- sources/sdk/xfer_udp.c
+++ sources/sdk/xfer_udp.c
@@ -33,7 +33,7 @@
 /** Connection handle for a UDP Server session */
 
 #include "xfer_udp.h"
-
+#include "math.h"
 extern struct netif server_netif;
 extern uint8_t rx_flag;
 extern uint8_t tx_flag;
@@ -45,7 +45,12 @@ static struct perf_stats server;
 
 static uint8_t PID;
 static uint8_t BITMAP[8192];
-static uint32_t TX_DUMMY[1024];
+static uint16_t *RESP_LIST; //array of SEG_NUMS to be transmitted
+
+static uint32_t TX_DUMMY[4096];
+static uint32_t *REQ_BITMAP;
+
+#define MAX_BURST 512
 
 enum{XFER_ST, XFER_DATA, XFER_BAR, XFER_BA,XFER_REQ,XFER_END};
 enum{IDLE, WAIT, SEND_DATA, SEND_BAR, END};
@@ -59,25 +64,65 @@ for (i=0;i<8192;i++)
 }
 }
 
-static void set_bitmap(uint32_t *REQBITMAP,uint16_t tot_len)
+static void set_TXDUMMY(uint32_t inc)
 {
-	uint16_t i;
-for (i=0;i<tot_len;i++)
+	uint32_t i;
+for (i=0;i<4096;i++)
 {
-	BITMAP[i]=REQBITMAP[i];
+	TX_DUMMY[i]=i+inc*1024;
 }
+}
+
+static uint16_t set_resp_list(uint32_t *BITMAP32,uint16_t *RESP_LIST,uint16_t NUM_SEG )
+{
+	uint32_t i,j=0;
+	uint32_t INDEX, OFFSET;
+	uint8_t DATA;
+for (i=0;i<NUM_SEG;i++)
+{
+	INDEX=i/32;
+	OFFSET=i%32;
+	DATA=(BITMAP32[INDEX]>>(OFFSET))&0x1;
+if (DATA)
+{
+	RESP_LIST[j]=i;
+	j++;
+	if (j>=MAX_BURST)
+	{
+		return MAX_BURST;
+	}
+}
+
+}
+return j;
 }
 
 static uint32_t count_bitmap(void )
 {
 	uint32_t i,SUM=0;
-for (i=0;i<65536;i++)
+for (i=0;i<8192;i++)
 {
-	if (BITMAP[i]==1)
-		SUM++;
+	SUM=SUM+__builtin_popcount(BITMAP[i]);
 }
+
 return SUM;
 }
+
+static uint32_t count_reqbitmap(uint32_t BYTEMAP_SIZE )
+{
+	uint32_t i,SUM=0,j;
+for (i=0;i<BYTEMAP_SIZE/4;i++)
+{
+		SUM=SUM+__builtin_popcount(REQ_BITMAP[i]);
+if ((i>1)&(SUM>10))
+{
+	j=0;
+}
+}
+
+return SUM;
+}
+
 
 static uint8_t isbitmap_full(uint16_t NUM_SEG)
 {
@@ -107,45 +152,54 @@ static void udp_recv_cb(void *arg, struct udp_pcb *tpcb,
 
 uint8_t MASK;
 uint16_t INDEX;
+uint16_t OFFSET;
 uint8_t BYTEMAP;
 uint32_t BA_HEADER=0x00000003;
 uint32_t REQ_HEADER=0x00000004;
 uint32_t END_HEADER=0x00000005;
 uint32_t DATA_HEADER=0x00000001;
 uint32_t HEADER;
-uint32_t BITMAP_SIZE;
+uint32_t BYTEMAP_SIZE;
+uint32_t temp1,temp2,temp3;
+uint16_t RESP_LENGTH;
 	PKT = p->payload;
 	CMD=PKT[0];
 
 	switch (CMD)
 	{
 	case XFER_REQ:
-		set_bitmap(&PKT[4],p->tot_len);
+		NUM_SEG=(PKT[0] >> 16) & 0x0000FFFF;
+		BYTEMAP_SIZE=NUM_SEG/8;
+		temp1=count_bitmap();
+		RESP_LIST=malloc(sizeof(uint16_t)*MAX_BURST);
 
+
+		if (NUM_SEG%8!=0) BYTEMAP_SIZE++;
+	//	REQ_BITMAP=malloc(sizeof(uint8_t)*BYTEMAP_SIZE);
+	//	memcpy(REQ_BITMAP, &PKT[1], sizeof(uint8_t)*BYTEMAP_SIZE);
+	//	temp2=count_bitmap();
+	//	temp3=count_reqbitmap(BYTEMAP_SIZE);
+		RESP_LENGTH=set_resp_list(&PKT[1],RESP_LIST,NUM_SEG);
 	 //   memcpy(pH->payload, &REQ_HEADER, sizeof(uint32_t));
 	 //   memcpy(pD->payload, TX_DUMMY, sizeof(uint32_t)*100);
 	  //  pbuf_cat(pH,pD);
-		NUM_SEG=(PKT[0] >> 16) & 0x0000FFFF;
 
-
-		for (i=0;i<NUM_SEG;i++)
+		for (i=0;i<RESP_LENGTH;i++)
 		{
-			SEG_NUM=i;
+			SEG_NUM=RESP_LIST[i];
 			INDEX=SEG_NUM/8;
-			MASK=1<<(SEG_NUM%8);
-			BYTEMAP=BITMAP[INDEX] & (MASK);
-				if (BYTEMAP==0)
-				{
-					HEADER=(DATA_HEADER&0x0000FFFF) | ((SEG_NUM << 16)&0xFFFF0000);
-					pH = pbuf_alloc(PBUF_TRANSPORT, sizeof(uint32_t), PBUF_RAM);
-					pD = pbuf_alloc(PBUF_TRANSPORT, sizeof(uint32_t)*1024, PBUF_RAM);
-					memcpy(pH->payload, &HEADER, sizeof(uint32_t));
-					 memcpy(pD->payload, TX_DUMMY, sizeof(uint32_t)*1024);
-						pbuf_cat(pH,pD);
-				    udp_sendto(tpcb, pH, addr, port);  //SEND BA
-					pbuf_free(pH);
+			OFFSET=(SEG_NUM%8);
+			MASK=1<<OFFSET;
 
-				}
+			set_TXDUMMY(RESP_LIST[i]);
+			HEADER=(DATA_HEADER&0x0000FFFF) | ((SEG_NUM << 16)&0xFFFF0000);
+			pH = pbuf_alloc(PBUF_TRANSPORT, sizeof(uint32_t), PBUF_RAM);
+			pD = pbuf_alloc(PBUF_TRANSPORT, sizeof(uint32_t)*1024, PBUF_RAM);
+			memcpy(pH->payload, &HEADER, sizeof(uint32_t));
+			 memcpy(pD->payload, TX_DUMMY, sizeof(uint32_t)*1024);
+				pbuf_cat(pH,pD);
+		    udp_sendto(tpcb, pH, addr, port);  //SEND BA
+			pbuf_free(pH);
 			}
 
 		//SEND RX_END
@@ -154,6 +208,7 @@ uint32_t BITMAP_SIZE;
 	    memcpy(pH->payload, &END_HEADER, sizeof(uint32_t));
 	    udp_sendto(tpcb, pH, addr, port);  //SEND BA
 		pbuf_free(pH);
+		free(RESP_LIST);
 
 
 		pbuf_free(p);
@@ -217,7 +272,7 @@ uint32_t i;
 	/* specify callback to use for incoming connections */
 	udp_recv(pcb, udp_recv_cb, NULL);
 
-	for (i=0;i<1024;i++)
+	for (i=0;i<4096;i++)
 	{
 		TX_DUMMY[i]=i;
 	}
